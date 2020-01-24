edfbe6fb3f8a7e534cea04a779fbb9cf2e476e84
Lovpreet Hansra <lhansra@ucsd.edu>
2020-01-23 17:30:14 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Lovpreet Hansra on 1/23/20.
+//  Copyright © 2020 Lovpreet Hansra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
